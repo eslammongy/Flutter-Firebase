f3edabb80dfa2a22ecbe650ad4d93de07b39f946
eslam <eslammongy995@gmail.com>
2024-03-06 08:36:52 +0200

    Finish SignInRepo Refactoring & Improve Code Readability

--- lib/features/signin/data/repos/signin_repo.dart
+++ lib/features/signin/data/repos/signin_repo.dart
@@ -3,8 +3,7 @@ import 'package:flutter_firebase/core/errors/exp_enum.dart';
 import 'package:flutter_firebase/features/profile/data/models/user_model.dart';
 
 abstract class SignInRepo {
-  Future<Either<AuthExceptionsTypes, UserModel?>> signInWithGoogle(
-      {required UserModel userModel});
+  Future<Either<AuthExceptionsTypes, UserModel?>> signInWithGoogle();
 
   Future<Either<AuthExceptionsTypes, UserModel>> signUpWithEmail(
       {required String email, required String password});
@@ -20,8 +19,8 @@ abstract class SignInRepo {
       required Function(String verifyCode) setVerificationCode,
       required Function() verificationFailed});
 
-  Future<Either<AuthExceptionsTypes, UserModel?>> signInWithPhoneNumber(
-      {required String otpCode,
-      required String verificationId,
-      required UserModel userModel});
+  Future<Either<AuthExceptionsTypes, UserModel?>> signInWithPhoneNumber({
+    required String otpCode,
+    required String verificationId,
+  });
 }
--- lib/features/signin/data/repos/singin_repo_impl.dart
+++ lib/features/signin/data/repos/singin_repo_impl.dart
@@ -1,7 +1,5 @@
 import 'package:dartz/dartz.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/foundation.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter_firebase/core/errors/exp_enum.dart';
@@ -16,8 +14,7 @@ class SignInRepoImplementation implements SignInRepo {
   });
 
   @override
-  Future<Either<AuthExceptionsTypes, UserModel?>> signInWithGoogle(
-      {required UserModel userModel}) async {
+  Future<Either<AuthExceptionsTypes, UserModel?>> signInWithGoogle() async {
     try {
       GoogleSignIn googleSignIn = GoogleSignIn();
       GoogleSignInAccount? googleAccount = await googleSignIn.signIn();
@@ -26,25 +23,15 @@ class SignInRepoImplementation implements SignInRepo {
       AuthCredential credential = GoogleAuthProvider.credential(
           idToken: authentication?.idToken,
           accessToken: authentication?.accessToken);
-      User? user;
-
-      await firebaseAuth.signInWithCredential(credential).then((value) {
-        user = value.user!;
-        debugPrint("additional user info=> ${value.additionalUserInfo}");
+      await firebaseAuth
+          .signInWithCredential(credential)
+          .then((userCredential) {
+        if (userCredential.user != null) {
+          final userModel = _fillUserModel(userCredential.user!);
+          return right(userModel);
+        }
       });
-
-      if (user != null) {
-        userModel = UserModel(
-            uId: user!.uid,
-            name: user!.displayName,
-            email: user!.email,
-            image: user!.photoURL,
-            phone: user!.phoneNumber);
-
-        return right(userModel);
-      } else {
-        return left(AuthExceptionsTypes.undefined);
-      }
+      return left(AuthExceptionsTypes.undefined);
     } on PlatformException catch (ex) {
       return left(AuthExceptionHandler.handleException(ex.message));
     } on FirebaseAuthException catch (error) {
@@ -52,29 +39,33 @@ class SignInRepoImplementation implements SignInRepo {
     }
   }
 
+  UserModel _fillUserModel(User user) {
+    final userModel = UserModel(
+        uId: user.uid,
+        name: user.displayName,
+        email: user.email,
+        image: user.photoURL,
+        phone: user.phoneNumber);
+    return userModel;
+  }
+
   @override
   Future<Either<AuthExceptionsTypes, UserModel>> signUpWithEmail(
       {required String email, required String password}) async {
     try {
-      var userModel = UserModel();
       await firebaseAuth
           .createUserWithEmailAndPassword(email: email, password: password)
           .then((userCredential) {
-        userModel = UserModel(
-          uId: userCredential.user!.uid,
-          name: userCredential.user?.displayName,
-          email: email,
-          phone: userCredential.user?.phoneNumber,
-          password: password,
-        );
+        if (userCredential.user != null) {
+          final userModel = _fillUserModel(userCredential.user!);
+          return right(userModel);
+        }
       });
-
-      return right(userModel);
+      return left(AuthExceptionsTypes.undefined);
     } on FirebaseAuthException catch (error) {
       return left(AuthExceptionHandler.handleException(error.code));
     } catch (error) {
-      return left(
-          AuthExceptionHandler.handleException(AuthExceptionsTypes.undefined));
+      return left(AuthExceptionsTypes.undefined);
     }
   }
 
@@ -82,25 +73,18 @@ class SignInRepoImplementation implements SignInRepo {
   Future<Either<AuthExceptionsTypes, UserModel?>> signInWithEmailPass(
       {required String email, required String password}) async {
     try {
-      var user = (await firebaseAuth.signInWithEmailAndPassword(
-              email: email, password: password))
-          .user;
-      if (user != null) {
-        var userModel = UserModel(
-            uId: user.uid,
-            name: user.displayName,
-            email: user.email,
-            image: user.photoURL,
-            phone: user.phoneNumber);
-
-        return right(userModel);
-      } else {
-        return left(AuthExceptionsTypes.undefined);
-      }
+      await firebaseAuth
+          .signInWithEmailAndPassword(email: email, password: password)
+          .then((userCredential) {
+        if (userCredential.user != null) {
+          final userModel = _fillUserModel(userCredential.user!);
+          return right(userModel);
+        }
+      });
+      return left(AuthExceptionsTypes.undefined);
     } on FirebaseAuthException catch (error) {
       return left(AuthExceptionHandler.handleException(error.code));
     } catch (error) {
-      debugPrint("when user sign in => ${error.toString()}");
       return left(
           AuthExceptionHandler.handleException(AuthExceptionsTypes.undefined));
     }
@@ -150,28 +134,23 @@ class SignInRepoImplementation implements SignInRepo {
   }
 
   @override
-  Future<Either<AuthExceptionsTypes, UserModel?>> signInWithPhoneNumber(
-      {required String otpCode,
-      required String verificationId,
-      required UserModel userModel}) async {
+  Future<Either<AuthExceptionsTypes, UserModel?>> signInWithPhoneNumber({
+    required String otpCode,
+    required String verificationId,
+  }) async {
     try {
       PhoneAuthCredential phoneAuthCredential = PhoneAuthProvider.credential(
           verificationId: verificationId, smsCode: otpCode);
-      var user =
-          (await firebaseAuth.signInWithCredential(phoneAuthCredential)).user;
+      await firebaseAuth
+          .signInWithCredential(phoneAuthCredential)
+          .then((userCredential) {
+        if (userCredential.user != null) {
+          final userModel = _fillUserModel(userCredential.user!);
+          return right(userModel);
+        }
+      });
 
-      if (user != null) {
-        userModel = UserModel(
-            uId: user.uid,
-            image: "",
-            phone: user.phoneNumber,
-            password: "",
-            email: "");
-        debugPrint("User Phone Number ${userModel.phone}");
-        return right(userModel);
-      } else {
-        return left(AuthExceptionsTypes.undefined);
-      }
+      return left(AuthExceptionsTypes.undefined);
     } on FirebaseAuthException catch (error) {
       return left(AuthExceptionHandler.handleException(error.code));
     } catch (error) {
